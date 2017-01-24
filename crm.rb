5d84cb49ce35a1b73d4655e0895e192882dbf09a
Pawel Parafianowicz <pawelp@outlook.com>
2017-01-24 14:38:40 -0500

    Complete

--- crm.rb
+++ crm.rb
@@ -1,6 +1,3 @@
-# Implement the new web-based CRM here.
-# Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
-# You'll have to implement it from scratch.
 require 'sinatra'
 require_relative 'contact'
 
@@ -17,6 +14,11 @@ get '/contacts' do
   erb :contacts
 end
 
-get '/new_contact' do
+get '/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
