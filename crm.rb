0ecf2595957b7a0a2bd0e77cf8ddf80aed2e445f
Pawel Parafianowicz <pawelp@outlook.com>
2017-01-25 15:51:57 -0500

    Complete individual search from list of contacts

--- crm.rb
+++ crm.rb
@@ -18,6 +18,11 @@ get '/new' do
   erb :new_contact
 end
 
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  erb :show_contact
+end
+
 post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
